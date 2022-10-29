674538524d4a3e00b998d343ff23758e959a7434
kevinnkim <71705286+kevinnkim@users.noreply.github.com>
2022-10-29 22:03:38 +0800

    29/10 commit
    changes only made in com/cognixia.config/SecurityConfig.java

--- user-service/src/main/java/com/cognixia/config/SecurityConfig.java
+++ user-service/src/main/java/com/cognixia/config/SecurityConfig.java
@@ -3,10 +3,13 @@ package com.cognixia.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,11 +22,24 @@ public class SecurityConfig{
 	@Autowired
 	private UserDetailsService userDetailService;
 	
+//	@Bean
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.userDetailsService(userDetailService).passwordEncoder(encodePwd());
+//	}
 	@Bean
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailService).passwordEncoder(encodePwd());
+	protected AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+	    return authConfig.getAuthenticationManager();
 	}
 	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+	    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+	     
+	    authProvider.setUserDetailsService(userDetailService);
+	    authProvider.setPasswordEncoder(encodePwd());
+	 
+	    return authProvider;
+	}
 //	@Override
 //	protected void configure(HttpSecurity http) throws Exception {
 //		http.csrf().disable();
@@ -32,11 +48,20 @@ public class SecurityConfig{
 	
 	@Bean
 	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
 		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/rest/**").authenticated().anyRequest().permitAll().and().formLogin();
+		http.authenticationProvider(authenticationProvider());
+		http.authorizeRequests().antMatchers("/rest/**").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin();
 		
 		return http.build();
 	}
+	
+	@Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
+    }
 	
 	@Bean
 	public BCryptPasswordEncoder encodePwd()
